eaf740ee08f16cd7c7ef08dd463e20b3ec2cde3f
kimjeahoon <hoon@steppay.kr>
2022-12-14 18:30:12 +0900

    cache clear

---
+++ packages/sub1/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      sub module12312312 &nbsp; <span>{count}</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/sub2/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      sub module 3asdsssf &nbsp; <span>{count}</span>
+    </div>
+  );
+}
+
+export default App;
